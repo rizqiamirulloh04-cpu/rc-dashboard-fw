eb9691a4ec3ac45fec71e2cb111b129af1e970b1
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-18 03:35:24 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,22 @@
 #include <Arduino.h>
 
-int throttle = 0;
-int steering = 0;
-
-void drawDashboard() {
-    Serial.print("Throttle: ");
-    Serial.print(throttle);
-
-    Serial.print(" | Steering: ");
-    Serial.println(steering);
-}
-
 void setup() {
     Serial.begin(115200);
 
-    Serial.println();
-    Serial.println("RC Dashboard Starting...");
+    Serial.println("RC Dashboard Start");
 }
 
 void loop() {
+    static int speed = 0;
 
-    throttle += 5;
-    steering += 3;
+    Serial.print("Speed: ");
+    Serial.println(speed);
 
-    if (throttle > 100) throttle = 0;
-    if (steering > 100) steering = 0;
+    speed++;
 
-    drawDashboard();
+    if(speed > 200) {
+        speed = 0;
+    }
 
-    delay(200);
+    delay(100);
 }
