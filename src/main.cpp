efd95ab9eac714bcebbc28686c295e265c2ace9b
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-18 03:18:41 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <Arduino.h>
 
+int throttle = 0;
+int steering = 0;
+
+void drawDashboard() {
+    Serial.print("Throttle: ");
+    Serial.print(throttle);
+
+    Serial.print(" | Steering: ");
+    Serial.println(steering);
+}
+
 void setup() {
     Serial.begin(115200);
+
+    Serial.println();
+    Serial.println("RC Dashboard Starting...");
 }
 
 void loop() {
-    Serial.println("ESP32-C6 OK");
-    delay(1000);
+
+    throttle += 5;
+    steering += 3;
+
+    if (throttle > 100) throttle = 0;
+    if (steering > 100) steering = 0;
+
+    drawDashboard();
+
+    delay(200);
 }
