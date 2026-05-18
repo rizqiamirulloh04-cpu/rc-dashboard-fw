57268b13e62645c40e25a0cc5f2b6fad71f4d097
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-18 11:03:14 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
-#include <stdio.h>
+#include <Arduino.h>
+#include <TFT_eSPI.h>
 
-extern "C" void app_main(void)
+TFT_eSPI tft = TFT_eSPI();
+
+void setup()
 {
-    printf("ESP32-C6 Dashboard Start\n");
+    pinMode(22, OUTPUT);
+    digitalWrite(22, HIGH);
+
+    tft.init();
+    tft.setRotation(1);
+
+    tft.fillScreen(TFT_BLACK);
+
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.setTextSize(2);
+
+    tft.drawString("RC DASHBOARD", 40, 40);
+    tft.drawString("ESP32-C6", 70, 80);
+
+    tft.drawRect(20, 140, 280, 30, TFT_WHITE);
+    tft.fillRect(20, 140, 180, 30, TFT_GREEN);
+
+    tft.drawString("Throttle", 90, 180);
+}
+
+void loop()
+{
 }
