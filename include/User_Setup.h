bf3de75001295122446f3856371a4e77f9616af3
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-18 11:02:09 +0700

    Create include/User_Setup.h

---
+++ include/User_Setup.h
@@ -0,0 +1,13 @@
+#define ST7789_DRIVER
+
+#define TFT_WIDTH 172
+#define TFT_HEIGHT 320
+
+#define TFT_MOSI 6
+#define TFT_SCLK 7
+#define TFT_CS   14
+#define TFT_DC   15
+#define TFT_RST  21
+#define TFT_BL   22
+
+#define SPI_FREQUENCY 40000000
