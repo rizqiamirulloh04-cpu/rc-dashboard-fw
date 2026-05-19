29ddaba7d08f03093fb93098386af1b24de3e143
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-20 01:14:31 +0700

    Update waveshare_lcd.h

--- include/waveshare_lcd.h
+++ include/waveshare_lcd.h
@@ -1,1 +1,36 @@
- 
+#ifndef WAVESHARE_LCD_H
+#define WAVESHARE_LCD_H
+
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+
+#define LCD_WIDTH 172
+#define LCD_HEIGHT 320
+
+#define LCD_BL 22
+
+TFT_eSPI tft = TFT_eSPI();
+
+void lcd_init()
+{
+    pinMode(LCD_BL, OUTPUT);
+
+    digitalWrite(LCD_BL, HIGH);
+
+    tft.begin();
+
+    tft.setRotation(1);
+
+    tft.fillScreen(TFT_BLACK);
+}
+
+void lcd_brightness(uint8_t brightness)
+{
+    ledcAttachPin(LCD_BL, 0);
+
+    ledcSetup(0, 5000, 8);
+
+    ledcWrite(0, brightness);
+}
+
+#endif
