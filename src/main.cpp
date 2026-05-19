5986be71704e5a73cb03d6348c9e9c8575494807
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-19 18:54:23 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,56 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
-// SPI display bus
+#define BLACK 0x0000
+#define WHITE 0xFFFF
+#define GREEN 0x07E0
+
+// SPI bus
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
-    8,   // DC
-    7,   // CS
-    6,   // SCK
-    10,  // MOSI
-    -1   // MISO
+15, // DC
+14, // CS
+7,  // SCK
+6,  // MOSI
+-1  // MISO
 );
 
-// ST7789 display
+// Display ST7789
 Arduino_GFX *gfx = new Arduino_ST7789(
-    bus,
-    3,    // RST
-    1,    // rotation
-    true, // IPS
-    172,  // width
-    320   // height
+bus,
+21,   // RST
+1,    // rotation
+true, // IPS
+172,  // width
+320   // height
 );
 
 void setup()
 {
-    pinMode(22, OUTPUT);
-    digitalWrite(22, HIGH);
+pinMode(22, OUTPUT);
+digitalWrite(22, HIGH);
 
-    gfx->begin();
+gfx->begin();  
 
-    gfx->fillScreen(BLACK);
+gfx->fillScreen(BLACK);  
 
-    gfx->setTextColor(GREEN);
-    gfx->setTextSize(2);
+gfx->setTextColor(GREEN);  
+gfx->setTextSize(2);  
 
-    gfx->setCursor(40, 40);
-    gfx->println("RC DASHBOARD");
+gfx->setCursor(25, 40);  
+gfx->println("RC DASHBOARD");  
 
-    gfx->setCursor(70, 80);
-    gfx->println("ESP32-C6");
+gfx->setCursor(50, 80);  
+gfx->println("ESP32-C6");  
 
-    gfx->drawRect(20, 140, 280, 30, WHITE);
-    gfx->fillRect(20, 140, 180, 30, GREEN);
+// Bar outline  
+gfx->drawRect(20, 140, 280, 30, WHITE);  
 
-    gfx->setCursor(90, 190);
-    gfx->println("Throttle");
+// Fill bar  
+gfx->fillRect(20, 140, 180, 30, GREEN);  
+
+gfx->setCursor(90, 190);  
+gfx->println("Throttle");
+
 }
 
 void loop()
