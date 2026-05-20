14857077fecc6e089fcf81b620d98c4792e0851d
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-20 18:00:58 +0700

    Update lv_conf.h

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -1,16 +1,27 @@
-#ifndef LV_CONF_H
-#define LV_CONF_H
+#pragma once
+
+#define LV_CONF_INCLUDE_SIMPLE 1
 
 #define LV_COLOR_DEPTH 16
 
-#define LV_MEM_SIZE (64U * 1024U)
+#define LV_USE_LOG 0
 
-#define LV_USE_PERF_MONITOR 1
+#define LV_USE_ASSERT_NULL 0
+#define LV_USE_ASSERT_MALLOC 0
+#define LV_USE_ASSERT_STYLE 0
+#define LV_USE_ASSERT_MEM_INTEGRITY 0
+#define LV_USE_ASSERT_OBJ 0
 
-#define LV_USE_SYSMON 1
+#define LV_USE_PERF_MONITOR 0
+#define LV_USE_MEM_MONITOR 0
 
-#define LV_FONT_MONTSERRAT_18 1
-#define LV_FONT_MONTSERRAT_32 1
-#define LV_FONT_MONTSERRAT_48 1
+#define LV_MEM_CUSTOM 0
 
-#endif
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_28 1
+
+#define LV_USE_LABEL 1
+#define LV_USE_BTN 1
+#define LV_USE_BAR 1
+#define LV_USE_ARC 1
