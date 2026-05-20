d8422ef2fc011c135bff8c10c6204b3b5f3167fa
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-20 19:19:32 +0700

    Update User_Setup.h

--- include/User_Setup.h
+++ include/User_Setup.h
@@ -1,1 +1,23 @@
-#pragma once
+#define ST7789_DRIVER
+
+#define TFT_WIDTH  172
+#define TFT_HEIGHT 320
+
+#define TFT_MOSI 6
+#define TFT_SCLK 7
+#define TFT_CS   14
+#define TFT_DC   15
+#define TFT_RST  21
+#define TFT_BL   22
+
+#define SPI_FREQUENCY  40000000
+
+#define TFT_RGB_ORDER TFT_RGB
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+#define SMOOTH_FONT
