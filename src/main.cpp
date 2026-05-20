d3cbb464240c23e148296bd801ebf1a80db31da0
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-21 01:10:48 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,107 @@
 #include <Arduino.h>
 #include <LovyanGFX.hpp>
 
-class LGFX : public lgfx::LGFX_Device {
-  lgfx::Panel_ST7789 _panel;
-  lgfx::Bus_SPI _bus;
+class LGFX : public lgfx::LGFX_Device
+{
+    lgfx::Panel_ST7789 _panel;
+    lgfx::Bus_SPI _bus;
+    lgfx::Light_PWM _light;
 
 public:
-  LGFX() {
+    LGFX(void)
     {
-      auto cfg = _bus.config();
+        {
+            auto cfg = _bus.config();
 
-      cfg.spi_host = SPI2_HOST;
-      cfg.spi_mode = 0;
-      cfg.freq_write = 40000000;
-      cfg.freq_read = 16000000;
+            cfg.spi_host = SPI2_HOST;
+            cfg.spi_mode = 0;
 
-      cfg.pin_sclk = 6;
-      cfg.pin_mosi = 7;
-      cfg.pin_miso = -1;
-      cfg.pin_dc   = 14;
+            cfg.freq_write = 40000000;
+            cfg.freq_read  = 16000000;
 
-      _bus.config(cfg);
-      _panel.setBus(&_bus);
+            cfg.spi_3wire = false;
+            cfg.use_lock  = true;
+
+            cfg.dma_channel = SPI_DMA_CH_AUTO;
+
+            cfg.pin_sclk = 7;
+            cfg.pin_mosi = 6;
+            cfg.pin_miso = -1;
+            cfg.pin_dc   = 15;
+
+            _bus.config(cfg);
+            _panel.setBus(&_bus);
+        }
+
+        {
+            auto cfg = _panel.config();
+
+            cfg.pin_cs   = 14;
+            cfg.pin_rst  = 21;
+            cfg.pin_busy = -1;
+
+            cfg.memory_width  = 172;
+            cfg.memory_height = 320;
+
+            cfg.panel_width  = 172;
+            cfg.panel_height = 320;
+
+            cfg.offset_x = 34;
+            cfg.offset_y = 0;
+
+            cfg.offset_rotation = 0;
+
+            cfg.readable   = false;
+            cfg.invert     = true;
+            cfg.rgb_order  = false;
+            cfg.dlen_16bit = false;
+            cfg.bus_shared = true;
+
+            _panel.config(cfg);
+        }
+
+        {
+            auto cfg = _light.config();
+
+            cfg.pin_bl = 22;
+            cfg.invert = false;
+
+            cfg.freq = 44100;
+            cfg.pwm_channel = 7;
+
+            _light.config(cfg);
+            _panel.setLight(&_light);
+        }
+
+        setPanel(&_panel);
     }
-
-    {
-      auto cfg = _panel.config();
-
-      cfg.pin_cs           = 15;
-      cfg.pin_rst          = 21;
-      cfg.pin_busy         = -1;
-
-      cfg.panel_width      = 172;
-      cfg.panel_height     = 320;
-
-      cfg.offset_x         = 34;
-      cfg.offset_y         = 0;
-
-      cfg.offset_rotation  = 0;
-      cfg.dummy_read_pixel = 8;
-      cfg.dummy_read_bits  = 1;
-
-      cfg.readable         = false;
-      cfg.invert           = false;
-      cfg.rgb_order        = false;
-      cfg.dlen_16bit       = false;
-      cfg.bus_shared       = true;
-
-      _panel.config(cfg);
-    }
-
-    setPanel(&_panel);
-  }
 };
 
 LGFX tft;
 
-void drawDashboard(int speed)
-{
-  tft.fillScreen(TFT_BLUE);
-
-  tft.fillRect(0, 0, 320, 80, TFT_BLUE);
-
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(3);
-
-  tft.setCursor(80, 20);
-  tft.printf("%d KM/H", speed);
-
-  tft.drawCircle(160, 220, 80, TFT_CYAN);
-  tft.drawCircle(160, 220, 81, TFT_CYAN);
-  tft.drawCircle(160, 220, 82, TFT_CYAN);
-
-  int angle = map(speed, 0, 100, -140, -40);
-
-  float rad = angle * 0.0174533;
-
-  int x = 160 + cos(rad) * 60;
-  int y = 220 + sin(rad) * 60;
-
-  tft.drawLine(160, 220, x, y, TFT_RED);
-  tft.fillCircle(160, 220, 8, TFT_WHITE);
-}
-
 void setup()
 {
-  tft.init();
+    tft.init();
 
-  tft.setRotation(1);
+    // Coba rotation 1
+    tft.setRotation(1);
 
-  drawDashboard(0);
+    tft.setBrightness(255);
+
+    tft.fillScreen(TFT_BLACK);
+
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextSize(3);
+
+    tft.drawString("WAVESHARE", 40, 40);
+
+    tft.setTextColor(TFT_GREEN);
+
+    tft.drawString("ESP32-C6", 55, 90);
+
+    tft.fillRect(20, 140, 280, 20, TFT_BLUE);
 }
 
 void loop()
 {
-  static int speed = 0;
-  static int dir = 1;
-
-  drawDashboard(speed);
-
-  speed += dir;
-
-  if(speed >= 100) dir = -1;
-  if(speed <= 0) dir = 1;
-
-  delay(30);
 }
