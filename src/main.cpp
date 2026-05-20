4dc65d9a679989669312d16177a510a4ac2db268
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-21 02:03:46 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 class LGFX : public lgfx::LGFX_Device
 {
-    lgfx::Panel_ST7789 _panel;
+    lgfx::Panel_ST7735S _panel;
     lgfx::Bus_SPI _bus;
     lgfx::Light_PWM _light;
 
@@ -11,16 +11,17 @@ public:
 
     LGFX(void)
     {
+        // ================= SPI =================
         {
             auto cfg = _bus.config();
 
             cfg.spi_host = SPI2_HOST;
             cfg.spi_mode = 0;
 
-            cfg.freq_write = 80000000;
-            cfg.freq_read  = 20000000;
+            cfg.freq_write = 40000000;
+            cfg.freq_read  = 16000000;
 
-            cfg.spi_3wire = true;
+            cfg.spi_3wire = false;
             cfg.use_lock = true;
             cfg.dma_channel = SPI_DMA_CH_AUTO;
 
@@ -33,37 +34,42 @@ public:
             _panel.setBus(&_bus);
         }
 
+        // ================= PANEL =================
         {
             auto cfg = _panel.config();
 
-            cfg.pin_cs           = 14;
-            cfg.pin_rst          = 21;
-            cfg.pin_busy         = -1;
+            cfg.pin_cs   = 14;
+            cfg.pin_rst  = 21;
+            cfg.pin_busy = -1;
 
-            cfg.memory_width     = 172;
-            cfg.memory_height    = 320;
+            cfg.memory_width  = 128;
+            cfg.memory_height = 160;
 
-            cfg.panel_width      = 172;
-            cfg.panel_height     = 320;
+            cfg.panel_width  = 128;
+            cfg.panel_height = 160;
 
-            cfg.offset_x         = 34;
-            cfg.offset_y         = 0;
-            cfg.offset_rotation  = 0;
+            cfg.offset_x = 0;
+            cfg.offset_y = 0;
 
-            cfg.readable         = false;
-            cfg.invert           = true;
-            cfg.rgb_order        = false;
-            cfg.dlen_16bit       = false;
-            cfg.bus_shared       = false;
+            cfg.offset_rotation = 0;
+
+            cfg.readable = false;
+            cfg.invert   = false;
+            cfg.rgb_order = false;
+
+            cfg.dlen_16bit = false;
+            cfg.bus_shared = false;
 
             _panel.config(cfg);
         }
 
+        // ================= BACKLIGHT =================
         {
             auto cfg = _light.config();
 
             cfg.pin_bl = 22;
             cfg.invert = false;
+
             cfg.freq = 44100;
             cfg.pwm_channel = 7;
 
@@ -88,15 +94,15 @@ void setup()
     tft.fillScreen(TFT_BLACK);
 
     tft.setTextColor(TFT_CYAN);
-    tft.setTextSize(3);
+    tft.setTextSize(2);
 
-    tft.drawCentreString("WAVESHARE", 160, 30, 2);
+    tft.drawString("WAVESHARE", 20, 20);
 
     tft.setTextColor(TFT_WHITE);
 
-    tft.drawCentreString("ESP32-C6", 160, 70, 2);
+    tft.drawString("ESP32-C6", 25, 50);
 
-    tft.fillRoundRect(30, 120, 260, 20, 10, TFT_GREEN);
+    tft.fillRect(10, 100, 140, 12, TFT_GREEN);
 }
 
 void loop()
