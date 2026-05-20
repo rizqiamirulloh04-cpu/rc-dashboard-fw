273b54959d5f7598b45e04de3cfa14a650fd49a7
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-21 00:50:08 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <lvgl.h>
 #include <LovyanGFX.hpp>
 
 class LGFX : public lgfx::LGFX_Device {
@@ -19,7 +18,7 @@ public:
       cfg.pin_sclk = 6;
       cfg.pin_mosi = 7;
       cfg.pin_miso = -1;
-      cfg.pin_dc   = 4;
+      cfg.pin_dc   = 14;
 
       _bus.config(cfg);
       _panel.setBus(&_bus);
@@ -28,12 +27,9 @@ public:
     {
       auto cfg = _panel.config();
 
-      cfg.pin_cs           = 5;
-      cfg.pin_rst          = 8;
+      cfg.pin_cs           = 15;
+      cfg.pin_rst          = 21;
       cfg.pin_busy         = -1;
-
-      cfg.memory_width     = 172;
-      cfg.memory_height    = 320;
 
       cfg.panel_width      = 172;
       cfg.panel_height     = 320;
@@ -46,7 +42,7 @@ public:
       cfg.dummy_read_bits  = 1;
 
       cfg.readable         = false;
-      cfg.invert           = true;
+      cfg.invert           = false;
       cfg.rgb_order        = false;
       cfg.dlen_16bit       = false;
       cfg.bus_shared       = true;
@@ -60,75 +56,31 @@ public:
 
 LGFX tft;
 
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[172 * 20];
-
-lv_obj_t *speedLabel;
-lv_obj_t *arc;
-
-void my_disp_flush(lv_disp_drv_t *disp,
-                   const lv_area_t *area,
-                   lv_color_t *color_p)
+void drawDashboard(int speed)
 {
-  uint32_t w = area->x2 - area->x1 + 1;
-  uint32_t h = area->y2 - area->y1 + 1;
+  tft.fillScreen(TFT_DARKBLUE);
 
-  tft.startWrite();
-  tft.pushImage(area->x1, area->y1, w, h,
-                (uint16_t *)&color_p->full);
-  tft.endWrite();
+  tft.fillRect(0, 0, 320, 80, TFT_BLUE);
 
-  lv_disp_flush_ready(disp);
-}
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(3);
 
-void create_dashboard()
-{
-  lv_obj_set_style_bg_color(
-      lv_scr_act(),
-      lv_color_hex(0x101828),
-      0);
+  tft.setCursor(80, 20);
+  tft.printf("%d KM/H", speed);
 
-  speedLabel = lv_label_create(lv_scr_act());
+  tft.drawCircle(160, 220, 80, TFT_CYAN);
+  tft.drawCircle(160, 220, 81, TFT_CYAN);
+  tft.drawCircle(160, 220, 82, TFT_CYAN);
 
-  lv_obj_set_style_text_color(
-      speedLabel,
-      lv_color_white(),
-      0);
+  int angle = map(speed, 0, 100, -140, -40);
 
-  lv_obj_set_style_text_font(
-      speedLabel,
-      &lv_font_montserrat_20,
-      0);
+  float rad = angle * 0.0174533;
 
-  lv_label_set_text(speedLabel, "0 KM/H");
+  int x = 160 + cos(rad) * 60;
+  int y = 220 + sin(rad) * 60;
 
-  lv_obj_align(speedLabel,
-               LV_ALIGN_TOP_MID,
-               0,
-               12);
-
-  arc = lv_arc_create(lv_scr_act());
-
-  lv_obj_set_size(arc, 100, 100);
-
-  lv_arc_set_rotation(arc, 135);
-  lv_arc_set_bg_angles(arc, 0, 270);
-  lv_arc_set_value(arc, 0);
-
-  lv_obj_set_style_arc_width(arc, 10, LV_PART_MAIN);
-  lv_obj_set_style_arc_width(arc, 10, LV_PART_INDICATOR);
-
-  lv_obj_set_style_arc_color(
-      arc,
-      lv_color_hex(0x00E5FF),
-      LV_PART_INDICATOR);
-
-  lv_obj_set_style_arc_color(
-      arc,
-      lv_color_hex(0x303030),
-      LV_PART_MAIN);
-
-  lv_obj_center(arc);
+  tft.drawLine(160, 220, x, y, TFT_RED);
+  tft.fillCircle(160, 220, 8, TFT_WHITE);
 }
 
 void setup()
@@ -137,31 +89,20 @@ void setup()
 
   tft.setRotation(1);
 
-  lv_init();
-
-  lv_disp_draw_buf_init(
-      &draw_buf,
-      buf,
-      NULL,
-      172 * 20);
-
-  static lv_disp_drv_t disp_drv;
-
-  lv_disp_drv_init(&disp_drv);
-
-  disp_drv.hor_res = 320;
-  disp_drv.ver_res = 172;
-
-  disp_drv.flush_cb = my_disp_flush;
-  disp_drv.draw_buf = &draw_buf;
-
-  lv_disp_drv_register(&disp_drv);
-
-  create_dashboard();
+  drawDashboard(0);
 }
 
 void loop()
 {
-  lv_timer_handler();
-  delay(5);
+  static int speed = 0;
+  static int dir = 1;
+
+  drawDashboard(speed);
+
+  speed += dir;
+
+  if(speed >= 100) dir = -1;
+  if(speed <= 0) dir = 1;
+
+  delay(30);
 }
