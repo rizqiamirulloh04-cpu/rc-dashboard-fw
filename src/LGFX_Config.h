c39504db1782c605345d1985153dfd7a26dc896b
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-22 00:08:12 +0700

    Update LGFX_Config.h

--- src/LGFX_Config.h
+++ src/LGFX_Config.h
@@ -20,8 +20,8 @@ public:
             cfg.spi_host = SPI2_HOST;
 
             cfg.spi_mode = 0;
-            cfg.freq_write = 80000000;
-            cfg.freq_read  = 20000000;
+            cfg.freq_write = 40000000;
+            cfg.freq_read  = 16000000;
 
             cfg.spi_3wire = true;
 
@@ -41,24 +41,33 @@ public:
         {
             auto cfg = _panel.config();
 
-            cfg.pin_cs           = 14;
-            cfg.pin_rst          = 21;
-            cfg.pin_busy         = -1;
+            cfg.pin_cs   = 14;
+            cfg.pin_rst  = 21;
+            cfg.pin_busy = -1;
 
-            cfg.memory_width     = 172;
-            cfg.memory_height    = 320;
-            cfg.panel_width      = 172;
-            cfg.panel_height     = 320;
+            cfg.memory_width  = 172;
+            cfg.memory_height = 320;
 
-            cfg.offset_x         = 34;
-            cfg.offset_y         = 0;
-            cfg.offset_rotation  = 0;
+            cfg.panel_width  = 172;
+            cfg.panel_height = 320;
 
-            cfg.readable         = false;
-            cfg.invert           = true;
-            cfg.rgb_order        = false;
-            cfg.dlen_16bit       = false;
-            cfg.bus_shared       = true;
+            cfg.offset_x = 34;
+            cfg.offset_y = 0;
+
+            cfg.offset_rotation = 0;
+
+            cfg.dummy_read_pixel = 8;
+            cfg.dummy_read_bits  = 1;
+
+            cfg.readable = false;
+
+            cfg.invert = false;
+
+            cfg.rgb_order = true;
+
+            cfg.dlen_16bit = false;
+
+            cfg.bus_shared = true;
 
             _panel.config(cfg);
         }
