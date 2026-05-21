df79affb2dd59373680d75ef7337d38c65a2d169
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-21 23:09:54 +0700

    Update lv_conf.h

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -3,12 +3,17 @@
 
 #define LV_COLOR_DEPTH 16
 
+#define LV_MEM_CUSTOM 0
 #define LV_MEM_SIZE (64U * 1024U)
 
 #define LV_USE_LOG 0
 
 #define LV_USE_PERF_MONITOR 1
 
-#define LV_TICK_CUSTOM 0
+#define LV_USE_LABEL 1
+#define LV_USE_BTN 1
+#define LV_USE_BAR 1
+#define LV_USE_ARC 1
+#define LV_USE_IMG 1
 
 #endif
