cda2055a16eafdef930261d12651c099f9a6ed88
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-22 00:08:40 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,3 @@
-#include <Arduino.h>
-#include <lvgl.h>
-#include "LGFX_Config.h"
-
-LGFX lcd;
-
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[172 * 20];
-
-void my_flush_cb(lv_disp_drv_t *disp,
-                 const lv_area_t *area,
-                 lv_color_t *color_p)
-{
-    int w = area->x2 - area->x1 + 1;
-    int h = area->y2 - area->y1 + 1;
-
-    lcd.startWrite();
-    lcd.setAddrWindow(area->x1, area->y1, w, h);
-    lcd.writePixels((lgfx::rgb565_t *)&color_p->full, w * h);
-    lcd.endWrite();
-
-    lv_disp_flush_ready(disp);
-}
-
 void setup()
 {
     Serial.begin(115200);
@@ -30,7 +6,16 @@ void setup()
 
     lcd.setBrightness(255);
 
-    lcd.setRotation(1);
+    lcd.setRotation(3);
+
+    lcd.fillScreen(TFT_RED);
+    delay(1000);
+
+    lcd.fillScreen(TFT_GREEN);
+    delay(1000);
+
+    lcd.fillScreen(TFT_BLUE);
+    delay(1000);
 
     lcd.fillScreen(TFT_BLACK);
 
@@ -54,10 +39,6 @@ void setup()
 
     lv_disp_drv_register(&disp_drv);
 
-    lv_obj_set_style_bg_color(lv_scr_act(),
-                              lv_color_hex(0x000000),
-                              0);
-
     lv_obj_t *label = lv_label_create(lv_scr_act());
 
     lv_label_set_text(label,
@@ -72,10 +53,4 @@ void setup()
                                0);
 
     lv_obj_center(label);
-}
-
-void loop()
-{
-    lv_timer_handler();
-    delay(5);
 }
