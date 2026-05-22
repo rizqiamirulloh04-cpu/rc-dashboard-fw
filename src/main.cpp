d0008242fb4809c0bb61d37877363a182e17ae02
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-23 01:12:20 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,39 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <Arduino_GFX_Library.h>
 #include <lvgl.h>
+#include <Arduino_GFX_Library.h>
+#include "waveshare_lcd.h"
 
-#define TFT_BL 15
+#define SCREEN_WIDTH 172
+#define SCREEN_HEIGHT 320
 
-// =======================
-// LCD CONFIG
-// =======================
+#define BLACK 0x0000
 
-Arduino_DataBus *bus = new Arduino_ESP32SPI(
-    8,    // DC
-    14,   // CS
-    6,    // SCK
-    7,    // MOSI
-    -1    // MISO
-);
+Arduino_DataBus *bus = new Arduino_HWSPI(
+    TFT_DC,
+    TFT_CS,
+    SCK,
+    MOSI,
+    MISO);
 
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
-    21,   // RST
-    1,    // rotation
-    true, // IPS
-    172,  // width
-    320,  // height
-    34,   // col offset
-    0,    // row offset
-    0     // col offset2
-);
-
-// =======================
-// LVGL BUFFER
-// =======================
+    TFT_RST,
+    0,
+    true,
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT);
 
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[172 * 20];
+static lv_color_t buf[SCREEN_WIDTH * 20];
 
-lv_disp_drv_t disp_drv;
+lv_obj_t *speedLabel;
+lv_obj_t *statusLabel;
+lv_obj_t *barThrottle;
+lv_obj_t *barSteering;
 
-// =======================
-// FLUSH CALLBACK
-// =======================
-
-void my_disp_flush(
-    lv_disp_drv_t *disp,
-    const lv_area_t *area,
-    lv_color_t *color_p)
+void my_disp_flush(lv_disp_drv_t *disp,
+                   const lv_area_t *area,
+                   lv_color_t *color_p)
 {
     uint32_t w = area->x2 - area->x1 + 1;
     uint32_t h = area->y2 - area->y1 + 1;
@@ -60,122 +48,106 @@ void my_disp_flush(
     lv_disp_flush_ready(disp);
 }
 
-// =======================
-// UI
-// =======================
-
-lv_obj_t *speedLabel;
-lv_obj_t *arc;
-
 void create_ui()
 {
     lv_obj_set_style_bg_color(
         lv_scr_act(),
         lv_color_hex(0x000000),
-        0);
+        LV_PART_MAIN);
 
-    // SPEED ARC
-    arc = lv_arc_create(lv_scr_act());
-
-    lv_obj_set_size(arc, 150, 150);
-
-    lv_arc_set_rotation(arc, 135);
-
-    lv_arc_set_bg_angles(arc, 0, 270);
-
-    lv_arc_set_range(arc, 0, 180);
-
-    lv_arc_set_value(arc, 108);
-
-    lv_obj_center(arc);
-
-    lv_obj_set_style_arc_width(
-        arc,
-        10,
-        LV_PART_INDICATOR);
-
-    lv_obj_set_style_arc_color(
-        arc,
-        lv_palette_main(LV_PALETTE_BLUE),
-        LV_PART_INDICATOR);
-
-    // SPEED TEXT
     speedLabel = lv_label_create(lv_scr_act());
-
-    lv_label_set_text(speedLabel, "108");
 
     lv_obj_set_style_text_color(
         speedLabel,
-        lv_color_white(),
-        0);
+        lv_color_hex(0x00FFAA),
+        LV_PART_MAIN);
 
     lv_obj_set_style_text_font(
         speedLabel,
-        &lv_font_montserrat_48,
-        0);
+        &lv_font_montserrat_28,
+        LV_PART_MAIN);
 
-    lv_obj_center(speedLabel);
-
-    // KMH
-    lv_obj_t *kmh = lv_label_create(lv_scr_act());
-
-    lv_label_set_text(kmh, "KM/H");
-
-    lv_obj_set_style_text_color(
-        kmh,
-        lv_palette_main(LV_PALETTE_CYAN),
-        0);
+    lv_label_set_text(speedLabel, "125");
 
     lv_obj_align(
-        kmh,
-        LV_ALIGN_CENTER,
-        0,
-        45);
-
-    // BATTERY
-    lv_obj_t *bat = lv_bar_create(lv_scr_act());
-
-    lv_obj_set_size(bat, 80, 10);
-
-    lv_bar_set_value(bat, 80, LV_ANIM_OFF);
-
-    lv_obj_align(
-        bat,
-        LV_ALIGN_TOP_RIGHT,
-        -10,
-        15);
-
-    // TITLE
-    lv_obj_t *title = lv_label_create(lv_scr_act());
-
-    lv_label_set_text(title, "ESP32-C6 RC");
-
-    lv_obj_set_style_text_color(
-        title,
-        lv_palette_main(LV_PALETTE_BLUE),
-        0);
-
-    lv_obj_align(
-        title,
+        speedLabel,
         LV_ALIGN_TOP_MID,
         0,
-        10);
-}
+        25);
 
-// =======================
-// SETUP
-// =======================
+    statusLabel = lv_label_create(lv_scr_act());
+
+    lv_obj_set_style_text_color(
+        statusLabel,
+        lv_color_hex(0xFFFFFF),
+        LV_PART_MAIN);
+
+    lv_obj_set_style_text_font(
+        statusLabel,
+        &lv_font_montserrat_16,
+        LV_PART_MAIN);
+
+    lv_label_set_text(statusLabel, "RC CONNECTED");
+
+    lv_obj_align(
+        statusLabel,
+        LV_ALIGN_TOP_MID,
+        0,
+        80);
+
+    barThrottle = lv_bar_create(lv_scr_act());
+
+    lv_obj_set_size(
+        barThrottle,
+        30,
+        140);
+
+    lv_obj_align(
+        barThrottle,
+        LV_ALIGN_BOTTOM_LEFT,
+        20,
+        -20);
+
+    lv_bar_set_range(
+        barThrottle,
+        0,
+        100);
+
+    lv_bar_set_value(
+        barThrottle,
+        80,
+        LV_ANIM_OFF);
+
+    barSteering = lv_bar_create(lv_scr_act());
+
+    lv_obj_set_size(
+        barSteering,
+        140,
+        20);
+
+    lv_obj_align(
+        barSteering,
+        LV_ALIGN_BOTTOM_MID,
+        20,
+        -30);
+
+    lv_bar_set_range(
+        barSteering,
+        -100,
+        100);
+
+    lv_bar_set_value(
+        barSteering,
+        35,
+        LV_ANIM_OFF);
+}
 
 void setup()
 {
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
-
-    delay(200);
+    Serial.begin(115200);
 
     gfx->begin();
-
-    gfx->fillScreen(BLACK);
+    gfx->fillScreen(0x0000);
 
     lv_init();
 
@@ -183,25 +155,23 @@ void setup()
         &draw_buf,
         buf,
         NULL,
-        172 * 20);
+        SCREEN_WIDTH * 20);
+
+    static lv_disp_drv_t disp_drv;
 
     lv_disp_drv_init(&disp_drv);
 
-    disp_drv.hor_res = 172;
-    disp_drv.ver_res = 320;
-
+    disp_drv.hor_res = SCREEN_WIDTH;
+    disp_drv.ver_res = SCREEN_HEIGHT;
     disp_drv.flush_cb = my_disp_flush;
-
     disp_drv.draw_buf = &draw_buf;
 
     lv_disp_drv_register(&disp_drv);
 
     create_ui();
-}
 
-// =======================
-// LOOP
-// =======================
+    Serial.println("Dashboard Started");
+}
 
 void loop()
 {
