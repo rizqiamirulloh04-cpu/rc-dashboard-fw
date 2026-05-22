35cdf7292c05fa1ac56c9e417b8e3e1a5ca8d992
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-23 01:55:10 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,32 @@
 #include <SPI.h>
 #include <Arduino_GFX_Library.h>
 
-#define TFT_BL   22
+// ======================
+// Waveshare ESP32-C6 LCD 1.47
+// ======================
 
+// Pin SPI
 #define TFT_SCLK 6
 #define TFT_MOSI 7
-#define TFT_DC   15
 #define TFT_CS   14
+#define TFT_DC   15
 #define TFT_RST  21
+#define TFT_BL   22
+
+// Warna RGB565
+#define BLACK 0x0000
+#define BLUE  0x001F
+#define RED   0xF800
+#define GREEN 0x07E0
+#define WHITE 0xFFFF
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
     TFT_DC,
     TFT_CS,
     TFT_SCLK,
     TFT_MOSI,
-    -1);
+    -1
+);
 
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
@@ -23,19 +35,23 @@ Arduino_GFX *gfx = new Arduino_ST7789(
     0,
     true,
     172,
-    320);
+    320
+);
 
 void setup()
 {
     Serial.begin(115200);
 
+    // Backlight
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH);
 
     gfx->begin();
 
+    // Rotation
     gfx->setRotation(1);
 
+    // Test warna
     gfx->fillScreen(BLACK);
     delay(1000);
 
@@ -48,12 +64,20 @@ void setup()
     gfx->fillScreen(BLUE);
     delay(1000);
 
+    // Teks
     gfx->fillScreen(BLACK);
 
-    gfx->setCursor(20, 120);
     gfx->setTextColor(WHITE);
-    gfx->setTextSize(2);
-    gfx->println("ESP32-C6 OK");
+    gfx->setTextSize(3);
+
+    gfx->setCursor(20, 80);
+    gfx->println("WAVESHARE");
+
+    gfx->setCursor(20, 130);
+    gfx->println("ESP32-C6");
+
+    gfx->setCursor(20, 180);
+    gfx->println("LCD OK!");
 }
 
 void loop()
