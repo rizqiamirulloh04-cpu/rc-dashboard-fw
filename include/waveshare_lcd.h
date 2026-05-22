290dc6ba2bac5971769ef7e9140935f9123a5fb0
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-23 01:26:03 +0700

    Update waveshare_lcd.h

--- include/waveshare_lcd.h
+++ include/waveshare_lcd.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#define TFT_MOSI 7
-#define TFT_SCLK 6
-#define TFT_CS   14
-#define TFT_DC   15
-#define TFT_RST  21
-#define TFT_BL   22
+#define TFT_BL   15
 
-#define MOSI TFT_MOSI
+#define TFT_SCLK 6
+#define TFT_MOSI 7
+#define TFT_DC   4
+#define TFT_CS   5
+#define TFT_RST  21
+
 #define SCK  TFT_SCLK
+#define MOSI TFT_MOSI
 #define MISO -1
