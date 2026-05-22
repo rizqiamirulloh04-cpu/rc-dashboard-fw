9675aa15c57258929dd14fd801de77e13932215c
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-22 22:43:37 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ Arduino_DataBus *bus = new Arduino_ESP32SPI(
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
     3,    // RST
-    2,    // rotation
+    3,    // rotation
     true, // IPS
     172,
     320,
