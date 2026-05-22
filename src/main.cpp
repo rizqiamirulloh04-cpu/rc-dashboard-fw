8151095fc54d3cad575348c06583dfa971550453
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-22 22:23:21 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,49 @@
 #include <Arduino.h>
+#include <Arduino_GFX_Library.h>
 
-#define TFT_BL 15
+#define GFX_BL 15
+
+Arduino_DataBus *bus = new Arduino_ESP32SPI(
+    6,   // DC
+    7,   // CS
+    5,   // SCK
+    4,   // MOSI
+    GFX_NOT_DEFINED
+);
+
+Arduino_GFX *gfx = new Arduino_ST7789(
+    bus,
+    3,    // RST
+    0,    // rotation
+    true, // IPS
+    172,
+    320,
+    34,
+    0,
+    0,
+    0
+);
 
 void setup()
 {
-    pinMode(TFT_BL, OUTPUT);
+    pinMode(GFX_BL, OUTPUT);
+    digitalWrite(GFX_BL, HIGH);
 
-    digitalWrite(TFT_BL, HIGH);
+    Serial.begin(115200);
 
-    delay(1000);
+    delay(200);
+
+    gfx->begin();
+
+    gfx->fillScreen(0x0000);
+
+    gfx->setCursor(20, 40);
+    gfx->setTextColor(0xFFFF);
+    gfx->setTextSize(2);
+    gfx->println("Waveshare ESP32-C6");
+
+    gfx->setCursor(20, 80);
+    gfx->println("LCD OK");
 }
 
 void loop()
