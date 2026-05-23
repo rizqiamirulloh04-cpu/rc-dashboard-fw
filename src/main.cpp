e4b4ae25ec02ba44e3845cc590891b4010c94b06
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-23 08:57:57 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,27 @@
 #include <SPI.h>
 #include <Arduino_GFX_Library.h>
 
-// ======================
-// Waveshare ESP32-C6 LCD 1.47
-// ======================
+#define GFX_BL 22
 
-// Pin SPI
-#define TFT_SCLK 6
-#define TFT_MOSI 7
-#define TFT_CS   14
-#define TFT_DC   15
-#define TFT_RST  21
-#define TFT_BL   22
-
-// Warna RGB565
-#define BLACK 0x0000
-#define BLUE  0x001F
-#define RED   0xF800
-#define GREEN 0x07E0
-#define WHITE 0xFFFF
-
+// SPI LCD
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
-    TFT_DC,
-    TFT_CS,
-    TFT_SCLK,
-    TFT_MOSI,
-    -1
+    15, // DC
+    14, // CS
+    6,  // SCK
+    7,  // MOSI
+    -1  // MISO
 );
 
+// Driver LCD
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
-    TFT_RST,
-    0,
-    false,
-    172,
-    320,
-    34,
-    0,
+    21,   // RST
+    0,    // rotation
+    true, // IPS
+    172,  // width
+    320,  // height
+    34,   // col offset
+    0,    // row offset
     0,
     0
 );
@@ -46,42 +31,36 @@ void setup()
 {
     Serial.begin(115200);
 
-    // Backlight
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
+    pinMode(GFX_BL, OUTPUT);
+    digitalWrite(GFX_BL, HIGH);
 
-    gfx->begin();
+    gfx->begin(40000000);
 
-    // Rotation
-    gfx->setRotation(3);
+    gfx->setRotation(1);
 
-    // Test warna
-    gfx->fillScreen(BLACK);
+    // test
+    gfx->fillScreen(0xF800);
     delay(1000);
 
-    gfx->fillScreen(RED);
+    gfx->fillScreen(0x07E0);
     delay(1000);
 
-    gfx->fillScreen(GREEN);
+    gfx->fillScreen(0x001F);
     delay(1000);
 
-    gfx->fillScreen(BLUE);
-    delay(1000);
+    gfx->fillScreen(0x0000);
 
-    // Teks
-    gfx->fillScreen(BLACK);
-
-    gfx->setTextColor(WHITE);
+    gfx->setTextColor(0xFFFF);
     gfx->setTextSize(3);
 
     gfx->setCursor(20, 80);
-    gfx->println("WAVESHARE");
-
-    gfx->setCursor(20, 130);
     gfx->println("ESP32-C6");
 
+    gfx->setCursor(20, 130);
+    gfx->println("WAVESHARE");
+
     gfx->setCursor(20, 180);
-    gfx->println("LCD OK!");
+    gfx->println("LCD TEST");
 }
 
 void loop()
