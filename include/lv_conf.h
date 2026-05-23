6c1cb3cf94c00125e98534f532c4642ac5e69cfb
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-23 10:58:08 +0700

    Update lv_conf.h

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -3,14 +3,13 @@
 
 #define LV_COLOR_DEPTH 16
 
-#define LV_MEM_SIZE (64U * 1024U)
+#define LV_MEM_CUSTOM 0
 
 #define LV_USE_LOG 0
 
-#define LV_USE_PERF_MONITOR 1
-
-#define LV_TICK_CUSTOM 1
-#define LV_TICK_CUSTOM_INCLUDE "Arduino.h"
-#define LV_TICK_CUSTOM_SYS_TIME_EXPR (millis())
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_18 1
+#define LV_FONT_MONTSERRAT_22 1
+#define LV_FONT_MONTSERRAT_28 1
 
 #endif
