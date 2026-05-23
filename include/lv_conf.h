67b388ebd651f425faed883ba8f1adab34e7901b
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-23 09:31:40 +0700

    Update lv_conf.h

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -7,16 +7,12 @@
 
 #define LV_USE_LOG 0
 
-#define LV_USE_PERF_MONITOR 1
+#define LV_USE_PERF_MONITOR 0
 
 #define LV_USE_LABEL 1
-#define LV_USE_BTN 1
 #define LV_USE_BAR 1
-#define LV_USE_ARC 1
+#define LV_USE_BTN 1
 
-#define LV_FONT_MONTSERRAT_12 1
-#define LV_FONT_MONTSERRAT_16 1
-#define LV_FONT_MONTSERRAT_20 1
-#define LV_FONT_MONTSERRAT_28 1
+#define LV_FONT_MONTSERRAT_22 1
 
 #endif
